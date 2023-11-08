784b24dc24233b37353aa8e3398628f1cd9077bb
Juan Augusto Haser <juanaugustohaser@gmail.com>
2023-11-08 01:22:08 -0300

    modificaciones en index, facturas, y css

--- ProyectoGAK/resources/views/facturas.blade.php
+++ ProyectoGAK/resources/views/facturas.blade.php
@@ -5,19 +5,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Facturas</title>
 
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="Stylesheet" href="{{ asset('css/facturas.css') }}">
     </head>
     <body>
         <header>
-            <ul class="navbar navbar-dark justify-content-between">
-              <li class="nav-item">
-                <a class="btn btn-outline-success" aria-current="page" href="#">Volver</a>
+            <ul class="nav justify-content-left">
+              <li class="nav-item my-2 mx-2">
+                <a class="btn btn-success" aria-current="page"><button onclick="ejec()">Volver</button></a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item titulo">
                 <h2 class="text-center">Venecia Modas</h2>
               </li>
             </ul>
-        </header>                  
+        </header>
+        <div>
+        </div>
+        <section>
+        </section>
     </body>
 </html>
